34958331eb80ffea3c9027d1ba072a1c6e28d2ef
fluorinedog <fluorinedog@gmail.com>
2018-11-15 02:35:38 +0800

    buggy

--- round_a.cpp
+++ round_a.cpp
@@ -1,70 +1,76 @@
 #include <iostream>
+#include <map>
 #include <string>
+#include <vector>
 using std::cin;
 using std::cout;
-using std::end;
+using std::endl;
+using std::make_pair;
+using std::map;
+using std::pair;
 using std::string;
+using std::vector;
+// record is to represent
+// N-digit number that satify the requirement
+using ll = long long;
+ll record[20][9] = {0};
 
-long long to_number(string str) {
-    return atoll(str.c_str());
-}
-
-string find_small(string num_str) {
-    for(int i = 0; i < num_str.size(); ++i) {
-        char ch = num_str[i];
-        auto digit = ch - '0';
-        if(digit % 2 == 1) {
-            num_str[i] = ch - 1;
-            ++i;
-            for(; i < num_str.size(); ++i) {
-                num_str[i] = '8';
-            }
-            break;
-        }
+void init() {
+    for(int i = 0; i < 9; ++i) {
+        record[0][0] = 0;
     }
-    return num_str;
-}
-
-string find_large(string num_str) {
-    for(int i = 0; i < num_str.size(); ++i) {
-        char ch = num_str[i];
-        auto digit = ch - '0';
-        if(digit % 2 == 1) {
-            if(digit != 9) {
-                num_str[i] = ch + 1;
-                ++i;
-                for(; i < num_str.size(); ++i) {
-                    num_str[i] = '0';
-                }
-                break;
-            } else {
-                // impossible
-                return "0";
+    record[0][0] = 1;
+    for(int i = 1; i < 20; ++i) {
+        for(int digit = 0; digit < 9; ++digit) {
+            for(int mod = 0; mod < 9; ++mod) {
+                record[i][(mod + digit) % 9] += record[i - 1][mod];
             }
         }
     }
-    return num_str;
+    // for(int i = 0; i < 20; ++i){
+    //     for(int j = 0; j < 9; ++j){
+    //         cout << record[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+}
+
+long long count(ll limit_num) {
+    if(limit_num == 0 || limit_num == 1) {
+        return 0;
+    }
+    auto limit = std::to_string(limit_num);
+    int N = limit.size();
+    ll total_count = 0;
+    int prefix_sum = 0;
+    // cout << "$" <<limit << endl;
+    for(int prefix_len = 0; prefix_len < N; ++prefix_len) {
+        int cur_digit = limit[prefix_len] - '0';
+        for(int digit = 0; digit < cur_digit; ++digit) {
+            for(int mod = 0; mod < 9; ++mod) {
+                if((mod + prefix_sum + digit) % 9 == 0) continue;
+                // cout << "<"  << N - 1 - prefix_len << "," << mod << ">";
+                total_count += record[N - 1 - prefix_len][mod];
+            }
+        }
+        prefix_sum += cur_digit;
+    }
+    return total_count;
 }
 
 long long workload() {
-    string num_str;
-    cin >> num_str;
-    auto num = to_number(num_str);
-    auto la = to_number(find_large(num_str));
-    auto sm = to_number(find_small(num_str));
-    if(la == 0){
-        return num - sm;
-    }
-    else {
-        return std::min(num - sm, la - num);
-    }
+    ll left, right;
+    cin >> left >> right;
+    return count(right + 1) - count(left);
 }
 
 int main() {
+    // freopen("input.txt", "r", stdin);
     int N;
     cin >> N;
-    for(int i = 0; i < N; ++i){
-       auto ans = workload();
-       cout << "Case #" << i + 1 << ": " << ans << std::endl;
+    init();
+    for(int i = 0; i < N; ++i) {
+        auto ans = workload();
+        cout << "Case #" << i + 1 << ": " << ans << std::endl;
     }
 }
--- round_b.cpp
+++ round_b.cpp
@@ -1,47 +1,106 @@
 #include <algorithm>
 #include <iomanip>
 #include <iostream>
+#include <map>
 #include <string>
 #include <tuple>
 #include <vector>
 using std::cin;
+using std::cerr;
 using std::cout;
 using std::endl;
+using std::map;
 using std::string;
+using std::tuple;
 using std::vector;
-
-std::tuple<double, int> new_expected(const vector<double>& prefix_sum,
-                                     const vector<double>& sorted_data, int N,
-                                     double expected, int hint) {
-    while(sorted_data[hint] < expected) {
-        hint++;
-    }
-    double right_sum = prefix_sum[N] - prefix_sum[hint];
-    double left_sum = expected * hint;
-    double aver = (right_sum + left_sum) / N;
-    return std::make_tuple(aver, hint);
+using ll = long long;
+int check(tuple<int, int> rule, int prefix) {
+    auto [len, count] = rule;
+    auto old_count = __builtin_popcount(prefix & ((1 << len) - 1));
+    return count - old_count;
 }
 
-double workload() {
-    int N, K;
-    cin >> N >> K;
-    vector<double> vec(N);
-    for(auto& x : vec) {
-        cin >> x;
+constexpr int max_diff = 8;
+constexpr int max_diff_count = 1 << max_diff;
+constexpr int MASK = max_diff_count - 1;
+ll counter(string preload, int str_len, map<int, vector<tuple<int, int>>>& constraints) {
+    vector<ll> record(max_diff_count, 0);
+    vector<ll> new_record(max_diff_count, 0);
+    int checkpoint = preload.size();
+    if(preload.size() > max_diff) {
+        preload = preload.substr(preload.size() - max_diff, max_diff);
     }
-    std::sort(vec.begin(), vec.end());
-    vector<double> prefix_sum(N + 1);
-    prefix_sum[0] = 0;
-    for(int i = 0; i < N; ++i) {
-        prefix_sum[i + 1] = prefix_sum[i] + vec[i];
+    int init_substr = 0;
+    for(auto ch : preload) {
+        if(ch == '1') {
+            init_substr = 2 * init_substr + 1;
+        } else {
+            init_substr = 2 * init_substr + 0;
+        }
     }
 
-    double expected = 0;
-    int hint = 0;
-    for(int i = 0; i < K + 1; ++i) {
-        std::tie(expected, hint) = new_expected(prefix_sum, vec, N, expected, hint);
+    for(auto rule: constraints[checkpoint]){
+        if(check(rule, init_substr) != 0){
+            cerr << "@";
+            return 0;
+        }
     }
-    return expected;
+
+    init_substr = MASK & (init_substr << 1);
+    ++checkpoint;
+
+    record[init_substr] = 1;
+    for(; checkpoint < str_len; checkpoint++) {
+        new_record.clear();
+        new_record.resize(max_diff_count, 0);
+        for(int substr = 0; substr < max_diff_count; substr++) {
+            bool flag_1 = true;
+            bool flag_0 = true;
+            for(auto rule : constraints[checkpoint]) {
+                int digit = check(rule, substr);
+                flag_1 = flag_1 && digit == 1;
+                flag_0 = flag_0 && digit == 0;
+            }
+            if(flag_1) {
+                new_record[MASK & ((substr << 1) | 1)] += record[substr];
+            }
+            if(flag_0) {
+                new_record[MASK & (substr << 1) | 0] += record[substr];
+            }
+        }
+        new_record.swap(record);
+    }
+    ll sum = 0;
+    for(int i = 0; i < max_diff_count; ++i) {
+        sum += record[i];
+    }
+    return sum;
+}
+
+string workload() {
+    int str_len, constraint_count;
+    ll rank;
+    cin >> str_len >> constraint_count >> rank;
+    rank--;
+    map<int, vector<tuple<int, int>>> limits;
+    for(int i = 0; i < constraint_count; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        limits[b - 1].emplace_back(b - a, c);
+    }
+    string prefix = "";
+    int sum = 0;
+    for(int i = 0; i < str_len; ++i) {
+        ll count = counter(prefix, str_len, limits);
+        cerr << i << "  %%" << rank << "$" << count << "#" << prefix << endl;
+        if(rank >= count) {
+            prefix += "1";
+            rank -= count;
+        } else {
+            prefix += "0";
+        }
+    }
+    return prefix;
 }
 
 int main() {
@@ -50,6 +109,8 @@ int main() {
     cin >> N;
     for(int i = 0; i < N; ++i) {
         auto ans = workload();
-        cout << "Case #" << i + 1 << ": " << std::fixed << std::setprecision(6) << ans << std::endl;
+        cerr << "checking case" << i << endl;
+        cout << "Case #" << i + 1 << ": " << std::fixed << std::setprecision(6) << ans
+             << std::endl;
     }
 }
