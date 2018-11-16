1bee1ce80d00d71dc5c17c1db27639814d9914f3
fluorinedog <fluorinedog@gmail.com>
2018-11-16 16:43:01 +0800

    buggy

--- round_c.cpp
+++ round_c.cpp
@@ -7,11 +7,13 @@
 #include <string>
 #include <tuple>
 #include <vector>
+
 using std::cerr;
 using std::cin;
 using std::cout;
 using std::endl;
 using std::make_tuple;
+using std::map;
 using std::pair;
 using std::set;
 using std::string;
@@ -19,135 +21,256 @@ using std::vector;
 
 using ll = long long;
 
-static constexpr int MAX = 1 << 20;
-class MonoidTree {
-  public:
-    using T = int;
-    MonoidTree() : data(2 * MAX, NIL) {}
-    void set(int loc, T value) {
-        assert(loc < MAX);
-        data[loc + MAX] = value;
-        int offset = loc + MAX;
-        while(offset >>= 1) {
-            data[offset] = FUNC(data[2 * offset], data[2 * offset + 1]);
+class FenwickTree {
+public:
+    explicit FenwickTree(int n) : data(n) {
+        for (int i = 0; i < n; ++i) {
+            data[i] = i;
         }
-    }
-    inline T FUNC(T a, T b) {
-        return a + b;
     }
 
-    T get(int loc){
-        return data[loc + MAX];
+    int find(int loc) const {
+        int iter = loc;
+        while (data[iter] != iter) iter = data[iter];
+        int parent = iter;
+        iter = loc;
+        while (data[iter] != iter) {
+            std::tie(iter, data[iter]) = make_tuple(data[iter], parent);
+        }
+        return parent;
     }
 
-    T reduce(int beg, int end) {
-        int bsize = MAX;
-        if(beg == end) {
-            return NIL;
+    bool merge(int prefer, int sub) {
+        prefer = find(prefer);
+        sub = find(sub);
+        if (sub == prefer) {
+            return false;
+        } else {
+            data[sub] = prefer;
+            return true;
         }
-        --end;
-        if(beg == end) {
-            return data[bsize + beg];
-        }
-        T left_sum = data[bsize + beg];
-        T right_sum = data[bsize + end];
-        while(end - beg != 1) {
-            if(!(beg & 1)) {
-                left_sum = FUNC(left_sum, data[bsize + beg + 1]);
-            }
-            if(end & 1) {
-                right_sum = FUNC(data[bsize + end - 1], right_sum);
-            }
-            beg >>= 1;
-            end >>= 1;
-            bsize >>= 1;
-        }
-        return FUNC(left_sum, right_sum);
     }
 
-  private:
-    vector<T> data;
-    static constexpr int NIL = 0;
+    bool isLinked(int a, int b) const {
+        return find(a) != find(b);
+    }
+
+private:
+    mutable vector<int> data;
 };
 
 class Solution {
-  public:
-    long long workload() {
+public:
+    Solution() {
         generate();
-        std::sort(dots.begin(), dots.end());
-        ll total_count = 0;
-        int last_v = -1;
-        int last_v_count = 0;
-        MonoidTree tree;
-        vector<pair<ll, ll>> record;
-        for(auto [this_v, h] : dots) {
-            if(this_v == last_v) {
-                ++last_v_count;
-            } else {
-                last_v_count = 0;
-                last_v = this_v;
-            }
-            tree.set(h, tree.get(h) + 1);
-            ll up = tree.reduce(0, h) - last_v_count;
-            ll down = tree.reduce(h + 1, MAX);
-            record.emplace_back(up, down);
-        }
-        last_v = -1;
-        tree = MonoidTree();
-        for(int i = dots.size(); i-- > 0;) {
-            auto [this_v, h] = dots[i];
-            if(this_v == last_v) {
-                ++last_v_count;
-            } else {
-                last_v_count = 0;
-                last_v = this_v;
-            }
-            tree.set(h, tree.get(h) + 1);
-            ll r_up = tree.reduce(0, h);
-            ll r_down = tree.reduce(h + 1, MAX) - last_v_count;
-            auto [up, down] = record[i];
-            total_count += r_up * down + r_down * up;
-            // cerr << "{" << this_v << ","<< h << "}";
-            // cerr << up << "^" << down << "^" << r_up << "^" << r_down << "^";
-        }
-        // cout << total_count;
-        return ((ll)N * (ll)(N - 1) * (N - 2)) / 6 - total_count;
+        gen_graph();
     }
 
-  private:
+    enum GridType {
+        Wall,
+        Potion,
+        Trap
+    };
+
+    GridType get_type(int id) {
+        ll value = matrix[id];
+        if (value == -100000) {
+            return Wall;
+        } else if (value < 0) {
+            return Trap
+        } else {
+            return Potion;
+        }
+    }
+
+    GridType get_type(int i, int j) {
+        int id = get_id(i, j);
+        return get_type(id);
+    }
+
+    void gen_graph() {
+        FenwickTree tree(N *M);
+        for (int i = 1; i < N - 1; ++i) {
+            for (int j = 1; j < M - 1; ++j) {
+                int grid = get_id(i, j);
+                int right_grid = get_id(i, j + 1);
+                int down_grid = get_id(i + 1, j);
+                if (get_type(grid) == Potion) {
+                    if (get_type(right_grid) == Potion) {
+                        tree.merge(grid, right_grid);
+                    }
+                    if (get_type(down_grid) == Potion) {
+                        tree.merge(grid, down_grid);
+                    }
+                }
+            }
+        }
+
+        map<int, int> mapping;
+        set<int> traps;
+        int trap_id_gen = 0;
+        int potion_id_gen = 15;
+        for (int i = 0; i < N - 1; ++i) {
+            for (int j = 1; j < M - 1; ++j) {
+                int grid = get_id(i, j);
+                auto type = get_type(grid);
+                switch (type) {
+                    case Wall:
+                        continue;
+                    case Potion: {
+                        int potion_id = tree.find(grid);
+                        if (!mapping.count(potion_id)) {
+                            mapping[potion_id] = potion_id_gen++;
+                        }
+                        potion_id = mapping[potion_id];
+                        value[potion_id] += matrix[grid];
+                        break;
+                    }
+                    case Trap: {
+                        int trap_id = grid;
+                        if (!mapping.count(trap_id)) {
+                            mapping[trap_id] = trap_id_gen++;
+                        }
+                        trap_id = mapping[trap_id];
+                        value[trap_id] += matrix[trap_id];
+                        traps.insert(trap_id);
+                        break;
+                    }
+                }
+            }
+        }
+
+        this->trap_count = trap_id_gen;   // todo
+        this->potion_count = potion_id_gen; // todo
+        for (auto trap: traps) {
+            auto diffs = {-M, +M, -1, +1};
+            for (auto diff:diffs) {
+                int neighbor = trap + diff;
+                if (get_type(neighbor) == Wall) continue;
+                int v1 = mapping[trap];
+                int v2 = mapping[neighbor];
+                graph[v1].insert(v2);
+                graph[v2].insert(v1);
+            }
+        }
+        auto[x, y] = source;
+        this->src_id = mapping[get_id(x, y)]; // todo
+        std::tie(x, y) = dest;
+        this->dst_id = mapping[get_id(x, y)]; // todo
+
+    }
+
+    ll calculate_life(const FenwickTree &tree){
+        ll life = init_life;
+        for(int i = 0; i < trap_count; ++i){
+            if(tree.isLinked(src_id, i)) {
+                life += value[i];
+            }
+        }
+        for(int i = 15; i < potion_count; ++i) {
+            if(tree.isLinked(src_id, i)) {
+                life += value[i];
+            }
+        }
+        return life;
+    }
+
+    void explore(const FenwickTree &last_tree, ll trap_vector) {
+        if(record.count(trap_vector)){
+            return;
+        }
+        auto life = calculate_life(last_tree);
+        record[trap_vector] = life;
+        if(last_tree.isLinked(src_id, dst_id)){
+            max_life = std::max(max_life, life);
+        }
+
+        int src_id = last_tree.find(this->src_id);
+        for (int trap_id = 0; trap_id < trap_count; ++trap_id) {
+            if (trap_vector & (1 << trap_id) == 1) {
+                continue;
+            }
+            for (auto nei: graph[trap_id]) {
+                if (last_tree.isLinked(nei, src_id)) {
+                    goto deal;
+                }
+            }
+            continue;
+            deal:
+            if (life < value[trap_id]) {
+                continue;
+            }
+            FenwickTree new_tree = last_tree;
+            ll new_vector = trap_vector | (1 << trap_id)
+            for (auto nei: graph[trap_id]) {
+                if( nei >= 15 || (1 << nei) & trap_vector == 1){
+                    new_tree.merge(nei, trap_id);
+                }
+            }
+            explore(new_tree, new_vector);
+        }
+    }
+
+    long long workload() {
+        FenwickTree init(potion_count + 15);
+        max_life = 0;
+        explore(init, 0);
+        return max_life;
+    }
+
+private:
+    int get_id(int n, int m) {
+        return n * M + m;
+    }
+
+    ll &get_raw_value(int n, int m) {
+        return matrix[get_id(n, m)];
+    }
+
     void generate() {
-        ll v, h;
-        cin >> N;
-        cin >> v >> h;
-        ll a, b, c, d, e, f;
-        ll m;
-        cin >> a >> b >> c;
-        cin >> d >> e >> f;
-        cin >> m;
-        dots.clear();
-        for(int i = 0; i < N; ++i) {
-            dots.emplace_back(v, h);
-            ll v_new = (a * v + b * h + c) % m;
-            ll h_new = (d * v + e * h + f) % m;
-            v = v_new;
-            h = h_new;
-            // cerr << "[" << v << "," << h << "]";
+        cin >> N >> M;
+        cin >> init_life;
+        cin >> source.first >> source.second;
+        cin >> dest.first >> dest.second;
+        N += 2;
+        M += 2;
+        matrix.resize(N * M, -100000);
+        for (int i = 1; i < N - 1; ++i) {
+            for (int j = 1; j < M - 1; ++j) {
+                cin >> get_raw_value(i, j);
+            }
         }
+
     }
 
-  private:
-    vector<pair<int, int>> dots;
-    int N;
+private:
+    int N, M;
+    ll init_life;
+    pair<int, int> source;
+    pair<int, int> dest;
+    vector<ll> matrix;
+
+private:
+    map<int, ll> record;
+    ll max_life;
+private:
+    int trap_count;
+    int potion_count;
+    int src_id;
+    int dst_id;
+    map<int, set<int>> graph; // todo
+    map<int, ll> value; // todo
+
 };
 
 int main() {
     int N;
     cin >> N;
-    for(int i = 0; i < N; ++i) {
+    for (int i = 0; i < N; ++i) {
         auto ans = Solution().workload();
         cout << "Case #" << i + 1
              << ": "                 //
-                                     //  << std::fixed << std::setprecision(6) //
+             //  << std::fixed << std::setprecision(6) //
              << ans << std::endl;    //
     }
 }
