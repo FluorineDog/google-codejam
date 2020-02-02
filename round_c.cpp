ec13100591031fdd44d7ee9ca01d391aebe4df4e
fluorinedog <fluorinedog@gmail.com>
2020-02-03 00:45:54 +0800

    update

--- round_c.cpp
+++ round_c.cpp
@@ -21,266 +21,76 @@ using std::vector;
 
 using ll = long long;
 
-class FenwickTree {
-public:
-    explicit FenwickTree(int n) : data(n) {
-        for (int i = 0; i < n; ++i) {
-            data[i] = i;
-        }
-    }
-
-    int find(int loc) const {
-        int iter = loc;
-        while (data[iter] != iter) iter = data[iter];
-        int parent = iter;
-        iter = loc;
-        while (data[iter] != iter) {
-            std::tie(iter, data[iter]) = make_tuple(data[iter], parent);
-        }
-        return parent;
-    }
-
-    bool merge(int prefer, int sub) {
-        prefer = find(prefer);
-        sub = find(sub);
-        if (sub == prefer) {
-            return false;
-        } else {
-            data[sub] = prefer;
-            return true;
-        }
-    }
-
-    bool isLinked(int a, int b) const {
-        return find(a) == find(b);
-    }
-
-private:
-    mutable vector<int> data;
-};
-
+constexpr ll mod = 1000000007;
 class Solution {
 public:
     Solution() {
     }
 
-    enum GridType {
-        Wall,
-        Potion,
-        Trap
-    };
-
-    GridType get_type(int id) {
-        ll value = matrix[id];
-        if (value == -100000) {
-            return Wall;
-        } else if (value < 0) {
-            return Trap;
-        } else {
-            return Potion;
+    void gen_twos(ll max){
+        if(twos.size() == 0){
+            twos.push_back(1);
+        }
+        while(twos.size() <= max){
+            ll acc = twos.back() * 2 % mod;
+            twos.push_back(acc);
+        }
+    }
+    void gen_facts(ll max){
+        if(facts.size() == 0){
+            facts.push_back(1);
+        }
+        while(facts.size() <= max){
+            ll acc = facts.back() * (ll)facts.size() % mod;
+            facts.push_back(acc);
+        }
+    }
+    ll fact(ll i){
+        return facts[i];
+    }
+    static ll power(ll base, ll exp){
+        if(exp == 0){
+            return 1;
+        }
+        else {
+            ll next = power(base, exp / 2);
+            next = next * next % mod;
+            if(exp % 2) return base * next % mod;
+            else return next;
         }
     }
 
-    GridType get_type(int i, int j) {
-        int id = get_id(i, j);
-        return get_type(id);
-    }
-
-    void gen_graph() {
-        FenwickTree tree(N *M);
-        for (int i = 1; i < N - 1; ++i) {
-            for (int j = 1; j < M - 1; ++j) {
-                int grid = get_id(i, j);
-                int right_grid = get_id(i, j + 1);
-                int down_grid = get_id(i + 1, j);
-                if (get_type(grid) == Potion) {
-                    if (get_type(right_grid) == Potion) {
-                        tree.merge(grid, right_grid);
-                    }
-                    if (get_type(down_grid) == Potion) {
-                        tree.merge(grid, down_grid);
-                    }
-                }
-            }
-        }
-
-        map<int, int> mapping;
-        set<int> traps;
-        int trap_id_gen = 0;
-        int potion_id_gen = 15;
-        for (int i = 0; i < N - 1; ++i) {
-            for (int j = 1; j < M - 1; ++j) {
-                int grid = get_id(i, j);
-                auto type = get_type(grid);
-                switch (type) {
-                    case Wall:
-                        continue;
-                    case Potion: {
-                        const int potion_id = tree.find(grid);
-                        if (!mapping.count(potion_id)) {
-                            mapping[potion_id] = potion_id_gen++;
-                        }
-                        value[mapping[potion_id]] += matrix[grid];
-                        break;
-                    }
-                    case Trap: {
-                        if (!mapping.count(grid)) {
-                            mapping[grid] = trap_id_gen++;
-                        }
-                        traps.insert(grid);
-                        value[mapping[grid]] += matrix[grid];
-                        break;
-                    }
-                }
-            }
-        }
-
-        this->trap_count = trap_id_gen;   // todo
-        this->potion_count = potion_id_gen; // todo
-        for (auto trap: traps) {
-            auto diffs = {-M, +M, -1, +1};
-            for (auto diff:diffs) {
-                int neighbor = tree.find(trap + diff);
-                if (get_type(neighbor) == Wall) continue;
-                int v1 = mapping[trap];
-                int v2 = mapping[neighbor];
-                graph[v1].insert(v2);
-                graph[v2].insert(v1);
-//                cerr << "(" << v1 << "->" << v2 << ")";
-            }
-        }
-//        cerr << endl;
-//        for(auto [k, v]: value){
-//
-//            cerr << "[" << k << "==>" << v << "]" << endl;
-//        }
-        auto[x, y] = source;
-        this->src_id = mapping[tree.find(get_id(x, y))]; // todo
-        std::tie(x, y) = dest;
-        this->dst_id = mapping[tree.find(get_id(x, y))]; // todo
-//        cerr << src_id << dst_id;
-    }
-
-    ll calculate_life(const FenwickTree &tree){
-        ll life = init_life;
-        for(int i = 0; i < trap_count; ++i){
-            if(tree.isLinked(src_id, i)) {
-                life += value[i];
-            }
-        }
-        for(int i = 15; i < potion_count; ++i) {
-            if(tree.isLinked(src_id, i)) {
-                life += value[i];
-            }
-        }
-        return life;
-    }
-
-    void explore(const FenwickTree &last_tree, const ll trap_vector){
-        if(record.count(trap_vector)){
-            return;
-        }
-        auto life = calculate_life(last_tree);
-        record[trap_vector] = life;
-//        cerr << "<" << trap_vector << "***" << life << ">" << endl;
-        if(last_tree.isLinked(src_id, dst_id)){
-            max_life = std::max(max_life, life);
-        }
-
-        const int src_id = last_tree.find(this->src_id);
-        for (int trap_id = 0; trap_id < trap_count; ++trap_id) {
-//            cerr  << "fucking" <<  trap_id << "at" << trap_vector << " ";
-            if ((trap_vector & (1 << trap_id)) != 0) {
-//                cerr << "skip dup" << endl;
-                continue;
-            }
-            for (auto nei: graph[trap_id]) {
-                if (last_tree.isLinked(nei, src_id)) {
-                    goto deal;
-                }
-            }
-//            cerr << "skip inner" << endl;
-            continue;
-            deal:
-            if (life + value[trap_id] < 0) {
-//                cerr << life  << "|"<< value[trap_id] << "|"<< trap_vector << endl;
-//                cerr << "skip lost life" << endl;
-                continue;
-            } else {
-//                cerr << life  << "&"<< value[trap_id] << "&"<< trap_vector << endl;
-            }
-            FenwickTree new_tree = last_tree;
-            ll new_vector = trap_vector | (1 << trap_id);
-            for (auto nei: graph.at(trap_id)) {
-                if( nei >= 15 || ((1 << nei) & trap_vector) != 0) {
-//                    cerr << "[" << nei << "]";
-                    new_tree.merge(nei, trap_id);
-                } else {
-//                    cerr << "<" << nei  << ">";
-                }
-            }
-//            cerr << endl;
-            explore(new_tree, new_vector);
-        }
+    ll modinv(ll a){
+        return power(a, mod - 2);
     }
 
     long long workload() {
         generate();
-        gen_graph();
-        FenwickTree init(potion_count);
-
-        max_life = -1;
-        explore(init, 0);
-        return max_life;
-    }
-
-private:
-    int get_id(int n, int m) {
-        return n * M + m;
-    }
-
-    ll &get_raw_value(int n, int m) {
-        return matrix[get_id(n, m)];
-    }
-
-    void generate() {
-        cin >> N >> M;
-        cin >> init_life;
-        cin >> source.first >> source.second;
-        cin >> dest.first >> dest.second;
-        N += 2;
-        M += 2;
-        matrix.resize(N * M, -100000);
-        for (int i = 1; i < N - 1; ++i) {
-            for (int j = 1; j < M - 1; ++j) {
-                cin >> get_raw_value(i, j);
-            }
+        gen_facts(N * 2);
+        gen_twos(couple_count);
+        ll up = 0;
+        ll down = fact(couple_count);
+        ll flag = 1;
+        for(ll pair = 0; pair <= couple_count; ++pair){
+            ll acc = fact(2 * N - pair) * modinv(fact(pair) * fact(couple_count - pair) % mod) % mod * twos[pair] % mod;
+            up = (up + flag * acc + mod) % mod;
+//            cerr << up * modinv(down) % mod << " ";
+            flag *=-1;
         }
-//        for(int i = 0; i < N; ++i){
-//            for(int j = 0; j < M; ++j){
-//                cerr << get_raw_value(i, j) << "\t";
-//            }
-//            cerr << endl;
-//        }
+//        cerr << endl;
+        return  up * down  % mod;
     }
 
 private:
-    int N, M;
-    ll init_life;
-    pair<int, int> source;
-    pair<int, int> dest;
-    vector<ll> matrix;
+    void generate() {
+        cin >> N >> couple_count;
+    }
 
 private:
-    map<int, ll> record;
-    ll max_life;
+    vector<ll> twos;
+    vector<ll> facts;
 private:
-    int trap_count;
-    int potion_count;
-    int src_id;
-    int dst_id;
-    map<int, set<int>> graph; // todo
-    map<int, ll> value; // todo
+    ll couple_count, N;
 
 };
 
@@ -290,8 +100,9 @@ int main() {
 
     int N;
     cin >> N;
+    Solution sol;
     for (int i = 0; i < N; ++i) {
-        auto ans = Solution().workload();
+        auto ans = sol.workload();
         cout << "Case #" << i + 1
              << ": "                 //
              //  << std::fixed << std::setprecision(6) //
---
+++ table.py
@@ -0,0 +1,8 @@
+#!python3
+x, y = input().split()
+x = int(x)
+y = int(y)
+table = {}
+for x in range(0, 10):
+    for y in range(0, 10):
+        
